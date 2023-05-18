7dd93b5cc17f5e1d9504e865c7aa9fe957251cbb
Ankit Agrawal <ankit.agarwal@intel.com>
2023-05-18 15:30:47 +0530

    Resolved Build Error in kernelflinger.
    We were getting following build errors-:
    Error-1: hardware/intel/kernelflinger/libkernelflinger/vbmeta_ias.c:199:40: error: incomplete definition of type 'struct evp_pkey_st'
    if (EVP_PKEY_RSA != EVP_PKEY_type(pkey->type)) {
    external/boringssl/include/openssl/base.h:424:16: note: forward declaration of 'struct evp_pkey_st'
    
    Solution-: instead of defining as reference pointer for pkey->type, use
    function method instead.
    
    Error-2: external/boringssl/include/openssl/base.h:333:9: error: unknown type name 'ptrdiff_t'
    Solution-: include stddef.h file to resolve cyclic dependency.
    
    Error-3: external/boringssl/include/openssl/bytestring.h:382:70: warning: declaration of 'struct tm' will not be visible outside of this function [-Wvisibility]
    Solution-: include declartion of this in header file.
    
    Original Jira - OAM-106853
    
    Tracked-On: OAM-112683
    Signed-off-by: Ankit Agrawal <ankit.agarwal@intel.com>

--- libkernelflinger/vbmeta_ias.c
+++ libkernelflinger/vbmeta_ias.c
@@ -196,7 +196,7 @@ static EVP_PKEY *get_rsa_pubkey(X509 *cert)
 	if (!pkey)
 		return NULL;
 
-	if (EVP_PKEY_RSA != EVP_PKEY_type(pkey->type)) {
+	if (EVP_PKEY_RSA != EVP_PKEY_type(EVP_PKEY_id(pkey))) {
 	        EVP_PKEY_free(pkey);
 		return NULL;
 	}
